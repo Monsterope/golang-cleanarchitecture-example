d9c8cbec1191e2b04635d99d435bceac85aec125
Monsterlove <monsterlove@Monsterloves-MacBook-Air-4.local>
2025-11-28 12:43:19 +0700

    feat: example pub/sub and cross sum env

--- configs/configs.go
+++ configs/configs.go
@@ -6,6 +6,30 @@ import (
 	"github.com/spf13/viper"
 )
 
+// //go:embed .env
+// var envFile embed.FS
+
+// func Load() {
+// 	content, err := envFile.ReadFile(".env")
+// 	if err != nil {
+// 		panic(fmt.Errorf("cannot read embedded env: %w", err))
+// 	}
+
+// 	viper.SetConfigType("env")
+// 	err = viper.ReadConfig(strings.NewReader(string(content)))
+// 	if err != nil {
+// 		panic(fmt.Errorf("cannot load env to viper: %w", err))
+// 	}
+
+// 	viper.AutomaticEnv()
+// }
+
+// func GetEnv(name string) string {
+// 	nameVarEnv := strings.ToUpper(name)
+// 	nameVarEnv = strings.ReplaceAll(nameVarEnv, ".", "_")
+// 	return viper.GetString(nameVarEnv)
+// }
+
 func Load() {
 	viper.SetConfigFile(".env")
 	// viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
--- pkg/utils/redis.go
+++ pkg/utils/redis.go
@@ -2,7 +2,9 @@ package utils
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
+	"log"
 	"time"
 
 	"github.com/redis/go-redis/v9"
@@ -86,3 +88,127 @@ func (s *RedisAuthStore) Set(key string, value interface{}) error {
 	}
 	return nil
 }
+
+func (s *RedisAuthStore) PushQueue(key string, value interface{}) error {
+	ctx := context.Background()
+	data, err := json.Marshal(value)
+	if err != nil {
+		return err
+	}
+	return s.clientAuth.LPush(ctx, key, data).Err()
+}
+
+// func (s *RedisAuthStore) PopQueue(key string) (string, error) {
+// 	ctx := context.Background()
+// 	result, err := s.clientAuth.RPop(ctx, key).Result()
+// 	if err == redis.Nil {
+// 		return "", nil // คิวว่าง
+// 	}
+// 	return result, err
+// }
+
+func (s *RedisAuthStore) PopQueue(key string) (string, error) {
+	ctx := context.Background()
+	result, err := s.clientAuth.BLPop(ctx, 0*time.Second, key).Result()
+	if err == redis.Nil {
+		return "", nil // คิวว่าง
+	}
+	if err != nil {
+		return "", err
+	}
+	// BLPop คืน slice [key, value] → เราเอา value มาใช้
+	if len(result) > 1 {
+		return result[1], nil
+	}
+	return "", err
+}
+
+func (s *RedisAuthStore) SubscriberEvent(ch string) *redis.PubSub {
+	ctx := context.Background()
+	return s.clientAuth.Subscribe(ctx, ch)
+}
+
+func (s *RedisAuthStore) StartSubscriber(ch string) {
+	ctx := context.Background()
+	pubsub := s.clientAuth.Subscribe(ctx, ch)
+	defer pubsub.Close()
+
+	log.Println("👂 Listening for messages on", ch)
+
+	for {
+		msg, err := pubsub.ReceiveMessage(ctx)
+		if err != nil {
+			log.Println("❌ Error receiving message:", err)
+			continue
+		}
+		// var result map[string][]interface{}
+		// if err := json.Unmarshal([]byte(msg.Payload), &result); err != nil {
+		// 	log.Println("unmarshal error:", err)
+		// 	continue
+		// }
+		fmt.Printf("📨 Received on %s: %s\n", msg.Channel, msg.Payload)
+	}
+
+}
+
+func (s *RedisAuthStore) StartPublisher(ch string, msg string) error {
+	ctx := context.Background()
+	err := s.clientAuth.Publish(ctx, ch, msg).Err()
+	if err != nil {
+		log.Println("❌ Publish error:", err)
+		return err
+	} else {
+		fmt.Println("📤 Published:", msg)
+		return nil
+	}
+	// for {
+	// 	message := fmt.Sprintf("Hello at %v", time.Now().Format(time.RFC3339))
+	// 	err := s.clientAuth.Publish(ctx, ch, message).Err()
+	// 	if err != nil {
+	// 		log.Println("❌ Publish error:", err)
+	// 	} else {
+	// 		fmt.Println("📤 Published:", message)
+	// 	}
+	// 	time.Sleep(3 * time.Second)
+	// }
+}
+
+// //example pub
+// func (r *ReceivedDataController) SendDataRealtime(num int, pubName string, data map[string]interface{}) {
+// 	jsonRealtime := make(map[int]interface{})
+// 	jsonRealtime[num] = data
+// 	jsonBytes, err := json.Marshal(jsonRealtime)
+// 	if err != nil {
+// 		log.Println("marshal error:", err)
+// 		return
+// 	}
+// 	jsonPayloadStr := string(jsonBytes)
+// 	if err := r.RedisStore.StartPublisher(fmt.Sprintf("%s_%v", pubName, num), jsonPayloadStr); err != nil {
+// 		log.Println("publish error:", err)
+// 		return
+// 	}
+// 	log.Println("publish success:")
+// }
+
+// //example sub
+// func (s *SubScriberDeviceController) PumpControl() {
+// 	ctx := context.Background()
+// 	pubsub := s.RedisStore.SubscriberEvent("data-send")
+// 	defer pubsub.Close()
+
+// 	log.Println("👂 Listening for messages on data-send")
+
+// 	for {
+// 		msg, err := pubsub.ReceiveMessage(ctx)
+// 		if err != nil {
+// 			log.Println("❌ Error receiving message:", err)
+// 			continue
+// 		}
+// 		var result map[string]interface{}
+// 		if errJson := json.Unmarshal([]byte(msg.Payload), &result); errJson != nil {
+// 			log.Println("unmarshal error:", errJson)
+// 			continue
+// 		}
+
+// 	}
+// }
